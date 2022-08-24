09d4df101c4c423159e6624dac3d006e6a764e8f
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-24 21:55:48 +0500

    Created AdminSeeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User();
+        $admin->name = 'Admin';
+        $admin->email = 'admin@influencer.com';
+        $admin->password = Hash::make('11111111');
+
+        $admin->save();
+
+        $admin->attachRole(1);
+    }
+}
