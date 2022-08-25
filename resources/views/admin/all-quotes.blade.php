65e40cde536f5fcaa75ec592cd46e82128088af6
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 20:07:59 +0500

    Designed all-quotes.blade.php

---
+++ resources/views/admin/all-quotes.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.table-layout')
+
+@section('title')
+    <title>All Quotes Display</title>
+@endsection
+
+@section('thead-area')
+
+    <th>ID</th>
+    <th>Quote</th>
+    <th>Author</th>
+    <th>Creation Date</th>
+    <th>Actions</th>
+
+@endsection
+
+@section('tbody-area')
+
+    @foreach($quotes as $quote)
+
+        <tr>
+            <td style="width: 20px">{{ $quote->id }}</td>
+            <td style="width: 50px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $quote->quote }}</td>
+            <td style="width: 50px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $quote->author }}</td>
+            <td style="width: 50px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $quote->created_at->format('d-M-Y') }}</td>
+            <td>
+                <div class="mr-2" style="display: inline-block">
+                    @include('buttons.edit-button')
+                </div>
+                <div style="display: inline-block">
+                    @include('buttons.delete-button')
+                </div>
+            </td>
+        </tr>
+
+    @endforeach
+
+@endsection
