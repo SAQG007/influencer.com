abc9d41aca4a2d440c1a1373ea720d56e12b762e
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 12:53:10 +0500

    Designed add-new-quote.blade.php

---
+++ resources/views/influencer/add-new-quote.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('title')
+    <title>Add New Quote</title>
+@endsection
+
+@section('content')
+
+    <div class="container" style="margin-top: 5%">
+        <form action="#" method="POST">
+            <div class="form-floating mb-3">
+                <textarea class="form-control" placeholder="Add your quote here" id="floatingTextarea"></textarea>
+                <label for="floatingTextarea">Add New Quote Here</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+@endsection
