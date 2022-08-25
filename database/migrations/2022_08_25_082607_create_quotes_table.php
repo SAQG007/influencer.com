d6a54964b4e6dd8da2b538c1e495a65a81014869
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 13:30:18 +0500

    Created Quotes Table

---
+++ database/migrations/2022_08_25_082607_create_quotes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->string('quote', 100);
+
+            // status can be active or inactive
+            $table->string('status', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
