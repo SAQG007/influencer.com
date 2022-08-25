6e186b6e614ecea48e14443bbc3ac6e54cf24168
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 21:38:06 +0500

    Added condition on button text

--- resources/views/admin/upsert-quote.blade.php
+++ resources/views/admin/upsert-quote.blade.php
@@ -7,17 +7,40 @@
 @section('content')
 
     <div class="container" style="margin-top: 5%">
-        <form class="needs-validation" action="#" method="POST">
+        <form method="POST"
+
+            @if($flag == "create")
+                action="{{ route('quote.store') }}"
+            @else
+                action="{{ route('quote.update', ['id' => $quote->id]) }}"
+            @endif
+
+        >
             @csrf
             <div class="form-floating mb-3">
-                <textarea name="quote" class="form-control" placeholder="Add your quote here" id="floatingTextarea"></textarea>
+                <textarea class="form-control" id="floatingTextarea" name="quote"
+                          placeholder="Add your quote here">@if($flag == "edit"){{ $quote->quote }}@endif</textarea>
                 <label for="floatingTextarea">Add New Quote Here</label>
             </div>
             <div class="form-floating mb-3">
-                <input name="author" type="text" class="form-control" id="floatingInput" placeholder="Quote Author (optional)">
+                <input class="form-control" id="floatingInput" name="author" placeholder="Quote Author (optional)"
+                       type="text"
+
+                    @if($flag == "edit")
+                        value="{{ $quote->author }}"
+                    @endif
+                >
                 <label for="floatingInput">Author (optional)</label>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">
+
+                @if($flag == "create")
+                    Save
+                @else
+                    Update
+                @endif
+
+            </button>
         </form>
     </div>
 
