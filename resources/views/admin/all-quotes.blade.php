b85e1aaeba22ff184f1e211508f65e0f57fbad31
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 20:24:17 +0500

    Added condition for no records found

--- resources/views/admin/all-quotes.blade.php
+++ resources/views/admin/all-quotes.blade.php
@@ -4,39 +4,45 @@
     <title>All Quotes Display</title>
 @endsection
 
-@section('thead-area')
+@if($quotes->count() == 0)
+    @section('thead-area')
+        <div class="text-muted text-center">No quotes found</div>
+    @endsection
+@else
+    @section('thead-area')
 
-    <th>ID</th>
-    <th>Quote</th>
-    <th>Author</th>
-    <th>Creation Date</th>
-    <th>Actions</th>
+        <th>ID</th>
+        <th>Quote</th>
+        <th>Author</th>
+        <th>Creation Date</th>
+        <th>Actions</th>
 
-@endsection
+    @endsection
 
-@section('tbody-area')
+    @section('tbody-area')
 
-    @foreach($quotes as $quote)
+        @foreach($quotes as $quote)
 
-        <tr>
-            <td style="width: 20px">{{ $quote->id }}</td>
-            <td style="width: 50px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $quote->quote }}</td>
-            <td style="width: 50px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $quote->author }}</td>
-            <td style="width: 50px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $quote->created_at->format('d-M-Y') }}</td>
-            <td>
-                <div class="mr-2" style="display: inline-block">
-                    @include('buttons.edit-button')
-                </div>
-                <div style="display: inline-block">
-                    @include('buttons.delete-button')
-                </div>
-            </td>
-        </tr>
+            <tr>
+                <td style="width: 20px">{{ $quote->id }}</td>
+                <td style="width: 50px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $quote->quote }}</td>
+                <td style="width: 50px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $quote->author }}</td>
+                <td style="width: 50px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $quote->created_at->format('d-M-Y') }}</td>
+                <td>
+                    <div class="mr-2" style="display: inline-block">
+                        @include('buttons.edit-button')
+                    </div>
+                    <div style="display: inline-block">
+                        @include('buttons.delete-button')
+                    </div>
+                </td>
+            </tr>
 
-    @endforeach
+        @endforeach
 
-@endsection
+    @endsection
 
-@section('pagination-area')
-    {{ $quotes->links() }}
-@endsection
+    @section('pagination-area')
+        {{ $quotes->links() }}
+    @endsection
+@endif
