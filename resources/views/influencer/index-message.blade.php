dcc21e5a65e08a66146221655e06e2068fa4feb8
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 12:17:17 +0500

    Designed index-message.blade.php

---
+++ resources/views/influencer/index-message.blade.php
@@ -0,0 +1,14 @@
+<div class="text-center fs-2" style="margin-top: 18%">
+    <div class="slideInDown" style="display: inline-block;">
+        Welcome to
+    </div>
+    <div class="slideInRight" style="display: inline-block; font-family: app-name-font">
+        <a href="{{ route('home') }}" style="text-decoration: none;">influencer.com</a>
+    </div>
+</div>
+
+<div class="text-center fs-5" style="font-family: Bahnschrift; color: red">
+    <div class="slideInLeft" style="display: inline-block">Winners</div>
+    <div class="slideInUp" style="display: inline-block">Never</div>
+    <div class="slideInRight" style="display: inline-block">Quit!</div>
+</div>
