5ff742c6affe00084978f14600c748d3a73b6603
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 14:02:33 +0500

    Minor Changes

--- resources/views/influencer/add-new-quote.blade.php
+++ resources/views/influencer/add-new-quote.blade.php
@@ -9,7 +9,7 @@
     <div class="container" style="margin-top: 5%">
         <form action="#" method="POST">
             <div class="form-floating mb-3">
-                <textarea class="form-control" placeholder="Add your quote here" id="floatingTextarea"></textarea>
+                <textarea name="quote" class="form-control" placeholder="Add your quote here" id="floatingTextarea"></textarea>
                 <label for="floatingTextarea">Add New Quote Here</label>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
