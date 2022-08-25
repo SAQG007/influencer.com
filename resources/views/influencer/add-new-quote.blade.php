c513808f048f599e5c6ee808845999110412758a
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 18:46:04 +0500

    Designed add-new-quote.blade.php

--- resources/views/influencer/add-new-quote.blade.php
+++ resources/views/influencer/add-new-quote.blade.php
@@ -7,10 +7,15 @@
 @section('content')
 
     <div class="container" style="margin-top: 5%">
-        <form action="#" method="POST">
+        <form class="needs-validation" action="#" method="POST">
+            @csrf
             <div class="form-floating mb-3">
                 <textarea name="quote" class="form-control" placeholder="Add your quote here" id="floatingTextarea"></textarea>
                 <label for="floatingTextarea">Add New Quote Here</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input name="author" type="text" class="form-control" id="floatingInput" placeholder="Quote Author (optional)">
+                <label for="floatingInput">Author (optional)</label>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
