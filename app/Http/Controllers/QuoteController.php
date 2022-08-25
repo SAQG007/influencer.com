c7674695c23e98015a8a5cc1d96715f466fc48c1
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 14:03:17 +0500

    Implemented store() function

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\Quotes;
 
 class QuoteController extends Controller
 {
     public function create()
     {
         return view('influencer.add-new-quote');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'quote' => 'required | max:100'
+        ]);
+
+        $quote = new Quotes();
+        $quote->quote = $request->input('quote');
+
+        if(User::verifyAdmin())
+        {
+            $quote->save();
+        }
     }
 }
