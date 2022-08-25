3236e20c4e3d75d9316810c65d36ea622f0f405e
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 18:26:20 +0500

    Added author column

--- database/migrations/2022_08_25_082607_create_quotes_table.php
+++ database/migrations/2022_08_25_082607_create_quotes_table.php
@@ -16,6 +16,7 @@ class CreateQuotesTable extends Migration
         Schema::create('quotes', function (Blueprint $table) {
             $table->id();
             $table->string('quote', 100);
+            $table->string('author', 50);
 
             // status can be active or inactive
             $table->string('status', 15)->default('active');
