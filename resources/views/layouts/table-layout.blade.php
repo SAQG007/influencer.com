7eedbff9428501c908143681909eaca373ae91f6
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 20:18:35 +0500

    Designed table-layout.blade.php

---
+++ resources/views/layouts/table-layout.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="container" style="margin-top: 3%; width: 80%;">
+        <div class="row">
+            <div class="col">
+                <table class="table table-striped table-hover table-bordered text-center"
+                       style="position: relative; table-layout: fixed">
+                    <thead class="table-dark">
+                        @yield('thead-area')
+                    </thead>
+
+                    <tbody>
+                        @yield('tbody-area')
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="row mb-4">
+            <div class="col" style="text-align: right">
+                @yield('pagination-area')
+            </div>
+        </div>
+    </div>
+
+@endsection
