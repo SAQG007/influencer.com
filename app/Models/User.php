83e9777bfaf49db3388d5429facb23992f8aebd2
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-25 14:03:58 +0500

    Implemented verifyAdmin() function

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 use Laratrust\Traits\LaratrustUserTrait;
 
@@ -43,4 +44,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function verifyAdmin()
+    {
+        if(Auth::user()->hasRole("Admin"))
+        {
+            return true;
+        }
+        return false;
+    }
 }
