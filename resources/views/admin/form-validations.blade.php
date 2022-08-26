81a383d1568d305fc7db91f27bee49d89974f41f
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-26 17:48:43 +0500

    Designed form-validations.blade.php

---
+++ resources/views/admin/form-validations.blade.php
@@ -0,0 +1,10 @@
+<span>
+    @error($fieldName)
+
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> {{ $message }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+
+    @enderror
+</span>
