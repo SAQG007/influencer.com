8fcb567d98c75d6539f1a32932a8192d25e93b3e
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-26 14:23:31 +0500

    Simplified code

--- resources/views/buttons/status-button.blade.php
+++ resources/views/buttons/status-button.blade.php
@@ -1,10 +1,5 @@
-<form action="{{ route('quote.status.change', ['id' => $quote->id]) }}" method="POST">
-    @csrf
-
-    @if($quote->status == "active")
-        <button class="btn btn-danger active" type="submit">Inactivate</button>
-    @else
-        <button class="btn btn-success active" type="submit">Activate</button>
-    @endif
-
-</form>
+@if($status == "active")
+    <button class="btn btn-danger active" type="submit">Inactivate</button>
+@else
+    <button class="btn btn-success active" type="submit">Activate</button>
+@endif
