82883cb939bcea5abab2356bd575c6d726fb6697
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-26 14:19:15 +0500

    Added and implemented index() function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::simplePaginate(10);
+
+        return view('admin.all-users')->with(['users' => $users]);
+    }
 }
