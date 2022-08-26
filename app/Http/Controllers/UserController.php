f92917201f4fd30f8fc0be6b2e0af684ec2ca237
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-26 15:50:23 +0500

    Added adn implemented store(), edit() and update() functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::simplePaginate(10);
+        $users = User::with('roles')->simplePaginate(10);
 
         return view('admin.all-users')->with(['users' => $users]);
     }
@@ -17,6 +21,43 @@ class UserController extends Controller
     public function create()
     {
         $flag = "create";
-        return view('admin.add-new-user')->with(['flag' => $flag]);
+        $roles = Role::all();
+        return view('admin.upsert-user')->with(['flag' => $flag, 'roles' => $roles]);
+    }
+
+    public function store(UserRequest $request)
+    {
+        $user = new User($request->all());
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+        $user->attachRole($request->input('role'));
+
+        return redirect()->route('users.all.show');
+    }
+
+    public function edit($id)
+    {
+        $flag = "edit";
+        $user = User::find($id);
+        $roles = Role::all();
+
+        return view('admin.upsert-user')->with(['flag' => $flag, 'user' => $user, 'roles' => $roles]);
+    }
+
+    public function update(UserRequest $request, $id)
+    {
+        $user = User::find($id);
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        $user->detachRole($user->roles[0]->id);
+        $user->attachRole($request->input('role'));
+
+        $user->password = Hash::make($request->input('password'));
+
+        $user->save();
+
+        return redirect()->route('users.all.show');
     }
 }
