dbc6d16b01d697bcf74f21dd126e5231c9987624
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-27 19:26:05 +0500

    Changed button

--- resources/views/mail/send-quote.blade.php
+++ resources/views/mail/send-quote.blade.php
@@ -1,17 +1,17 @@
 @component('mail::message')
 # Daily Quote
 
-<div class="card">
+<div class="card" @if($quote->language == "ur") style="direction: rtl" @endif>
     <div class="card-body">
         <blockquote class="blockquote mb-0">
             <p>{{ $quote->quote }}</p>
-            <footer class="blockquote-footer">{{ $quote->author }}</footer>
+            <footer class="blockquote-footer">- {{ $quote->author }}</footer>
         </blockquote>
     </div>
 </div>
 
-@component('mail::button', ['url' => ''])
-    Like Quote
+@component('mail::button', ['url' => '', 'color' => 'success'])
+    Add to favorites
 @endcomponent
 
 Thanks,<br>
