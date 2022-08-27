64a7b18fedee62f78cce662488db11ba354c5e23
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-27 20:35:46 +0500

    Implemented Urdu Language

--- resources/views/influencer/index-message.blade.php
+++ resources/views/influencer/index-message.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center fs-2" style="margin-top: 18%">
     <div class="slideInDown" style="display: inline-block;">
-        Welcome to
+        {{ __('messages.welcome_to') }}
     </div>
     <div class="slideInRight" style="display: inline-block; font-family: app-name-font">
         <a href="{{ route('home') }}" style="text-decoration: none;">influencer.com</a>
@@ -8,7 +8,7 @@
 </div>
 
 <div class="text-center fs-5" style="font-family: Bahnschrift; color: red">
-    <div class="slideInLeft" style="display: inline-block">Winners</div>
-    <div class="slideInUp" style="display: inline-block">Never</div>
-    <div class="slideInRight" style="display: inline-block">Quit!</div>
+    <div class="slideInLeft" style="display: inline-block">{{ __('messages.winners') }}</div>
+    <div class="slideInUp" style="display: inline-block">{{ __('messages.never') }}</div>
+    <div class="slideInRight" style="display: inline-block">{{ __('messages.quit') }}</div>
 </div>
