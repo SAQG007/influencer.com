71303b324e46643c3d33e8bd84f29967b8970935
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-27 20:56:04 +0500

    Implemented urdu language

--- resources/views/admin/upsert-quote.blade.php
+++ resources/views/admin/upsert-quote.blade.php
@@ -26,12 +26,13 @@
             @include('admin.form-validations', ['fieldName' => 'author'])
             @include('admin.form-validations', ['fieldName' => 'language'])
 
-            <div class="form-floating mb-3">
+            <div class="form-floating mb-3" @if($flag == "edit" && $quote->language == "en") style="direction: ltr" @endif>
                 <textarea class="form-control" id="floatingTextarea" name="quote"
                           placeholder="Add your quote here">@if($flag == "edit"){{ $quote->quote }}@endif</textarea>
-                <label for="floatingTextarea">Add New Quote Here</label>
+                <label for="floatingTextarea">{{ __('messages.add_new_quote_here') }}</label>
             </div>
-            <div class="form-floating mb-3">
+
+            <div class="form-floating mb-3" @if($flag == "edit" && $quote->language == "en") style="direction: ltr" @endif>
                 <input class="form-control" id="floatingInput" name="author" placeholder="Quote Author (optional)"
                        type="text"
 
@@ -39,28 +40,32 @@
                         value="{{ $quote->author }}"
                     @endif
                 >
-                <label for="floatingInput">Author (optional)</label>
+                <label for="floatingInput">{{ __('messages.author_optional') }}</label>
             </div>
 
-            <div class="row mt-3">
-                <h5>Quote Language</h5>
+            <div class="row mt-3" @if($flag == "edit" && $quote->language == "en") style="direction: ltr" @endif>
+                <h5>{{ __('messages.quote_lang') }}</h5>
                 <div class="input-group mb-3 inline" style="display: inline-block">
-                    <input type="radio" name="language" value="en" @if($flag == "edit" && $quote->language == "en") checked @endif> English
+                    <input type="radio" name="language" value="en" @if($flag == "edit" && $quote->language == "en") checked @endif> {{ __('messages.english') }}
                 </div>
                 <div class="input-group mb-3 inline" style="display: inline-block">
-                    <input type="radio" name="language" value="ur" @if($flag == "edit" && $quote->language == "ur") checked @endif> Urdu
+                    <input type="radio" name="language" value="ur" @if($flag == "edit" && $quote->language == "ur") checked @endif> {{ __('messages.urdu') }}
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-primary">
+            <div class="row">
+                <div class="col" @if($flag == "edit" && $quote->language == "en") style="direction: ltr;" @endif>
+                    <button type="submit" class="btn btn-primary">
 
-                @if($flag == "create")
-                    Save
-                @else
-                    Update
-                @endif
+                        @if($flag == "create")
+                            {{ __('messages.save') }}
+                        @else
+                            {{ __('messages.update') }}
+                        @endif
 
-            </button>
+                    </button>
+                </div>
+            </div>
         </form>
     </div>
 
