9509cfe02db64eecc4b095572b630e2d028c774f
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-27 20:34:31 +0500

    Created Urdu and English words file

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'change_language' => 'Change Language',
+    'edit_profile' => 'Edit Profile',
+    'add_new_user' => 'Add New User',
+    'subscribers_list' => 'Subscribers List',
+    'quotes_list' => 'Quotes List',
+    'add_new_quote' => 'Add New Quote',
+    'welcome_to' => 'Welcome to',
+    'winners' => 'Winners',
+    'never' => 'Never',
+    'quit' => 'Quit!',
+];
---
+++ resources/lang/ur/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'change_language' => 'زبان تبدیل کریں',
+    'edit_profile' => 'پروفائل میں ترمیم کریں',
+    'add_new_user' => 'نیا صارف شامل کریں',
+    'subscribers_list' => 'سبسکرائبرز کی فہرست',
+    'quotes_list' => 'اقتباس کی فہرست',
+    'add_new_quote' => 'نیا اقتباس شامل کریں',
+    'welcome_to' => 'خوش آمدید',
+    'winners' => 'فاتح',
+    'never' => 'کبھی',
+    'quit' => 'ہار نہیں مانتے!',
+];
