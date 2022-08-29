7bc52233747f2ad4f864f049a60195db79678408
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-29 15:32:42 +0500

    Added app logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,12 @@
     @endisset
 
     <x-auth-card>
-{{--        <x-slot name="logo">--}}
+        <x-slot name="logo">
 {{--            <a href="/">--}}
 {{--                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />--}}
 {{--            </a>--}}
-{{--        </x-slot>--}}
+            <img src="{{ asset('icons/motivation-icon.png') }}" width="60px" height="60px">
+        </x-slot>
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,11 @@
 <x-guest-layout>
     <x-auth-card>
-{{--        <x-slot name="logo">--}}
+        <x-slot name="logo">
 {{--            <a href="/">--}}
 {{--                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />--}}
 {{--            </a>--}}
-{{--        </x-slot>--}}
+            <img src="{{ asset('icons/motivation-icon.png') }}" width="60px" height="60px">
+        </x-slot>
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
