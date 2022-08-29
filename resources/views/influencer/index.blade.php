da33be6b0a0b57f5cdb20e19c2564ec35cd310ac
SAQG007 <syedabdulqadirgillani807@gmail.com>
2022-08-29 15:12:10 +0500

    Showed confirmation message

--- resources/views/influencer/index.blade.php
+++ resources/views/influencer/index.blade.php
@@ -1,12 +1,28 @@
-@extends('layouts.master')
+{{--@extends('layouts.master')--}}
 
-@section('title')
-    <title>Home</title>
+{{--@section('title')--}}
+{{--    <title>Home</title>--}}
 
-@endsection
+{{--@endsection--}}
 
-@section('content')
+{{--@section('content')--}}
 
-    @include('influencer.index-message')
+{{--    @include('influencer.index-message')--}}
 
-@endsection
+{{--@endsection--}}
+
+<!doctype html>
+<html>
+    <head>
+        <title>Home</title>
+
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    </head>
+
+    <body>
+    <div class="text-muted text-center" style="margin-top: 20%; font-size: 20pt">
+        You will receive email daily at 9 AM
+    </div>
+    </body>
+</html>
